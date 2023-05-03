78f756ec1948a0ace9294292abfe24099b73772d
JuliaRees <124360150+JuliaRees@users.noreply.github.com>
2023-05-03 17:11:38 +0800

    2pages

---
+++ scripts.js
@@ -0,0 +1,26 @@
+// A $( document ).ready() block.
+$( document ).ready(function() {
+    
+    $('.hamburger-button').click(function(){
+        $(this).toggleClass('active');
+        $(".mobile-menu").fadeToggle();
+      });
+
+// Animate on Scroll - activate
+AOS.init({
+    duration: 500
+});
+
+});
+
+lightGallery(document.getElementById('lightgallery'), {
+    speed: 500,
+    download: false
+    
+});
+
+lightGallery(document.getElementById('lightgallery-2'), {
+    speed: 500,
+    download: false
+    
+});
