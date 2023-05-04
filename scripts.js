f6d29987ae389dc625fe7da452023e321819326e
JuliaRees <124360150+JuliaRees@users.noreply.github.com>
2023-05-04 15:52:29 +0800

    fixes

--- scripts.js
+++ scripts.js
@@ -19,8 +19,28 @@ lightGallery(document.getElementById('lightgallery'), {
     
 });
 
-lightGallery(document.getElementById('lightgallery-2'), {
-    speed: 500,
-    download: false
-    
+// Slider Gallery //
+
+$('.slider-container').slick({
+	autoplay: true,       // Do we want it to autoplay? true or false
+	autoplaySpeed: 2000,  // How long between each slide when auto-playing
+	speed: 500,           // How fast is the transition in milliseconds
+	arrows: true,         // Do you want to show arrows to trigger each slide
+	accessibility: true,  // Enables keyboard tabbing and arrow key navigation
+	dots: true,           // Enables the dots below to show how many slides
+	fade: false,          // Changes the animate from slide to fade if true
+	infinite: true,      // When true, means that it will scroll in a circle
+	pauseOnHover: true,  // When true means the autoplay pauses when hovering
+	pauseOnDotsHover: true, // Pauses the autoplay when hovering over the dots
+  slidesToShow: 3,
+  slidesToScroll: 1,
+responsive: [
+    {
+      breakpoint: 768,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1
+      }
+    }
+  ]
 });
